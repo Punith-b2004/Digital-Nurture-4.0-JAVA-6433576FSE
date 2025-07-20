db3b05c04713da4e56bd4920da13dfbba7526bab
Punith B <puneethgowda91081@gmail.com>
2025-07-20 22:31:49 +0530

    Initial commit for Factory Method Pattern Example

--- Week-3/Spring Core_Maven/Exercise 2 - Implementing Dependency Injection/src/main/java/com/hemanth/repository/BookRepository.java
+++
@@ -1,5 +0,0 @@
-package com.bhave.repository;
-
-public interface BookRepository {
-    void save(String bookTitle);
-}
--- Week-3/Spring Core_Maven/Exercise 2 - Implementing Dependency Injection/src/main/java/com/hemanth/repository/BookRepositoryImpl.java
+++
@@ -1,8 +0,0 @@
-package com.bhave.repository;
-
-public class BookRepositoryImpl implements BookRepository {
-    @Override
-    public void save(String bookTitle) {
-        System.out.println("Saving book: " + bookTitle);
-    }
-}
